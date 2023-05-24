7bcc7f6d2e23a36322133a0076efea0daa988b40
Alba Garza <jarazethalba@gmail.com>
2023-05-24 00:10:48 -0600

    Added simple register state

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,20 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { FcGoogle } from "react-icons/fc";
 import { BsWindows, BsGithub } from "react-icons/bs";
 import "./Register.css";
 
 const Register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassowrd] = useState("");
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    //makes POST
+    console.log(email, password);
+  };
+
   return (
     <form>
       <div className="login-container">
-        <div className="card-login card-register">
+        <form className="card-login card-register">
           <h2 className="logo">Project One</h2>
           <label className="email-label">Enter your email: </label>
-          <input className="input-background" type="text" />
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            className="input-background"
+            type="text"
+          />
 
           <label className="email-label">Create a password: </label>
-          <input className="input-background" type="text" />
-          <button className="login-button">Continue</button>
+          <input
+            onChange={(e) => setPassowrd(e.target.value)}
+            className="input-background"
+            type="text"
+          />
+          <button
+            onClick={(e) => onSubmitHandler(e)}
+            type="submit"
+            className="login-button"
+          >
+            Continue
+          </button>
           <span>.................</span>
           <span>or</span>
           <div className="icons-container">
@@ -30,7 +53,7 @@ const Register = () => {
               </li>
             </ul>
           </div>
-        </div>
+        </form>
       </div>
     </form>
   );
