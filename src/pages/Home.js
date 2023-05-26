8bb3d322f63fac6b8ad76832a0979a2df83e4dd0
Alba Garza <jarazethalba@gmail.com>
2023-05-26 02:28:47 -0600

    Home dummy data for base

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,8 @@ import "./Home.css";
 
 const Home = () => {
   const [showMenu, setShowMenu] = useState(true);
+  const projects = ["preoject 1", "project 2", "project 3"];
+  const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
 
   const toggleMenu = () => {
     setShowMenu((prev) => !prev);
@@ -15,12 +17,30 @@ const Home = () => {
       <button onClick={toggleMenu}>---</button>
       {/* {showMenu && <SidePanel />} */}
       <SidePanel toggle={showMenu} />
-      <div className="right-nav ">
-        <MainNav />
-      </div>
-      <div className="content">
-        <h3>somn</h3>
-      </div>
+      <section className="home-content">
+        <h2>Current work</h2>
+        <div className="home-cards">
+          {projects.map((el) => {
+            return <div className="project-card">{el}</div>;
+          })}
+        </div>
+
+        <h2>This week</h2>
+        <div className="home-cards">
+          {days.map((el) => {
+            return <div className="day-card">{el}</div>;
+          })}
+        </div>
+
+        <h2>Your items</h2>
+        <div>
+          <ul>
+            <li>ticket #3</li>
+            <li>ticket #9</li>
+            <li>ticket #6</li>
+          </ul>
+        </div>
+      </section>
     </section>
   );
 };
