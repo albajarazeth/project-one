f8ed09e64803869e0631eb5ae8c0244f43ab1e56
Alba Garza <jarazethalba@gmail.com>
2023-05-26 01:22:17 -0600

    Made the panel toggle, got rid of grid

--- src/components/SidePanel.js
+++ src/components/SidePanel.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BsBoundingBoxCircles } from "react-icons/bs";
 import { MdDashboard } from "react-icons/md";
 import { AiFillCalendar } from "react-icons/ai";
@@ -6,34 +6,36 @@ import { BsChatSquareDotsFill } from "react-icons/bs";
 import { HiDocumentMinus } from "react-icons/hi2";
 import "./SidePanel.css";
 
-const SidePanel = () => {
+const SidePanel = ({ toggle }) => {
   return (
-    <section className="side-panel">
-      <div>
-        <ul className="panel-nav">
-          <div className="nav-link">
-            <BsBoundingBoxCircles className="nav-icon" />
-            <li>Projets</li>
-          </div>
-          <div className="nav-link">
-            <MdDashboard className="nav-icon" />
-            <li>Dashboard</li>
-          </div>
-          <div className="nav-link">
-            <AiFillCalendar className="nav-icon" />
-            <li>Calendar</li>
-          </div>
-          <div className="nav-link">
-            <BsChatSquareDotsFill className="nav-icon" />
-            <li>Chat</li>
-          </div>
-          <div className="nav-link">
-            <HiDocumentMinus className="nav-icon" />
-            <li>Documentation</li>
-          </div>
-        </ul>
-      </div>
-    </section>
+    <>
+      <nav className={`side-panel ${toggle ? "panel-open" : "panel-closed"}`}>
+        <div>
+          <ul className="panel-nav">
+            <div className="nav-link">
+              <BsBoundingBoxCircles className="nav-icon" />
+              <li>Projets</li>
+            </div>
+            <div className="nav-link">
+              <MdDashboard className="nav-icon" />
+              <li>Dashboard</li>
+            </div>
+            <div className="nav-link">
+              <AiFillCalendar className="nav-icon" />
+              <li>Calendar</li>
+            </div>
+            <div className="nav-link">
+              <BsChatSquareDotsFill className="nav-icon" />
+              <li>Chat</li>
+            </div>
+            <div className="nav-link">
+              <HiDocumentMinus className="nav-icon" />
+              <li>Documentation</li>
+            </div>
+          </ul>
+        </div>
+      </nav>
+    </>
   );
 };
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import SidePanel from "../components/SidePanel";
 import MainNav from "../components/MainNav";
 import "./Home.css";
 
 const Home = () => {
+  const [showMenu, setShowMenu] = useState(true);
+
+  const toggleMenu = () => {
+    setShowMenu((prev) => !prev);
+  };
+
   return (
-    <section className="grid-container">
-      <div className="left-nav">
-        <SidePanel />
-      </div>
+    <section className="home-container">
+      <button onClick={toggleMenu}>---</button>
+      {/* {showMenu && <SidePanel />} */}
+      <SidePanel toggle={showMenu} />
       <div className="right-nav ">
         <MainNav />
       </div>
