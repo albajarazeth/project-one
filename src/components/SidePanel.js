e0cf2e8fa1c449fbb3aa33d7bc62d93df1dca082
Alba Garza <jarazethalba@gmail.com>
2023-06-03 02:08:33 -0600

    Added pages and routes

--- src/components/SidePanel.js
+++ src/components/SidePanel.js
@@ -5,30 +5,48 @@ import { AiFillCalendar } from "react-icons/ai";
 import { BsChatSquareDotsFill } from "react-icons/bs";
 import { HiDocumentMinus } from "react-icons/hi2";
 import "./SidePanel.css";
+import { useNavigate } from "react-router-dom";
 
 const SidePanel = ({ toggle }) => {
+  const navigate = useNavigate();
+  const navigateToProjects = () => {
+    navigate("./projects");
+  };
+  const navigateToDashboard = () => {
+    navigate("./dashboard");
+  };
+  const navigateToCalendar = () => {
+    navigate("./calendar");
+  };
+  const navigateToChat = () => {
+    navigate("./chat");
+  };
+  const navigateToDocumentation = () => {
+    navigate("./documentation");
+  };
+
   return (
     <>
       <nav className={`side-panel ${toggle ? "panel-open" : "panel-closed"}`}>
         <div>
           <ul className="panel-nav">
-            <div className="nav-link">
+            <div onClick={navigateToProjects} className="nav-link">
               <BsBoundingBoxCircles className="nav-icon" />
               <li>Projets</li>
             </div>
-            <div className="nav-link">
+            <div onClick={navigateToDashboard} className="nav-link">
               <MdDashboard className="nav-icon" />
               <li>Dashboard</li>
             </div>
-            <div className="nav-link">
+            <div onClick={navigateToCalendar} className="nav-link">
               <AiFillCalendar className="nav-icon" />
               <li>Calendar</li>
             </div>
-            <div className="nav-link">
+            <div onClick={navigateToChat} className="nav-link">
               <BsChatSquareDotsFill className="nav-icon" />
               <li>Chat</li>
             </div>
-            <div className="nav-link">
+            <div onClick={navigateToDocumentation} className="nav-link">
               <HiDocumentMinus className="nav-icon" />
               <li>Documentation</li>
             </div>
--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -3,6 +3,11 @@ import { Route, Routes } from "react-router-dom";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import Home from "../pages/Home";
+import Projects from "../pages/Projects";
+import Dashboard from "../pages/Dashboard";
+import Calendar from "../pages/Calendar";
+import Chat from "../pages/Chat";
+import Documentation from "../pages/Documentation";
 
 const AppRoutes = () => {
   return (
@@ -10,6 +15,12 @@ const AppRoutes = () => {
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/" element={<Home />} />
+
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/calendar" element={<Calendar />} />
+      <Route path="/chat" element={<Chat />} />
+      <Route path="/documentation" element={<Documentation />} />
     </Routes>
   );
 };
